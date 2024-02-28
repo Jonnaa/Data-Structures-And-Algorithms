749926d26a275a5ff34dd150a1ea6aee6d10c2d5
Jonathan <Jonnaa96@yahoo.com>
2024-02-28 16:45:20 -0700

    Starting doubly linked list section | Constructor Lesson complete

---
+++ 4.doubly_linked_list/1_DLL_constructor.py
@@ -0,0 +1,30 @@
+############################################
+
+# Doubly Linked Lists Constructor Lesson
+
+############################################
+
+# Main change is that node class now requires a prev variable
+
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+my_doubly_linked_list = DoublyLinkedList(7)
+
+my_doubly_linked_list.print_list()
